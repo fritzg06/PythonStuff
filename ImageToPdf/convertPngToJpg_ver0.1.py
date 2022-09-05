15817220611cb473eebca6a0fe57a642a14ac110
Fritz Reyes <35794702+fritzg06@users.noreply.github.com>
2022-09-06 05:49:18 +0800

    new

--- ImageToPdf/convertPngToJpg_ver0.1.py
+++ ImageToPdf/convertPngToJpg_ver0.1.py
@@ -4,8 +4,8 @@
 import os
 from PIL import Image
 
-dirname_read="C:/Greenshot/PLUK Branch Assembly June 2021/png/"  # Note the trailing slash
-dirname_write="C:/Greenshot/PLUK Branch Assembly June 2021/jpg/"
+dirname_read="C:/GitHub/PythonStuff/ImageToPdf/"  # Note the trailing slash
+dirname_write="C:/GitHub/PythonStuff/ImageToPdf/"
 names=os.listdir(dirname_read)
 count=0
 for name in names:
